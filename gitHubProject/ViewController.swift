93d09607a1b5f22b3d2d928203d426fde92088d0
Sinan Yavi <>
2022-04-27 10:53:19 +0300

    first commit test

---
+++ gitHubProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  gitHubProject
+//
+//  Created by Sinan Yavi on 27.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        print("This is test for git")
+        print("new test")
+        print("test2")
+        
+    }
+
+
+}
+
